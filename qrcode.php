2f893236c4b397af97f4017529419b0ab6ec7de2
Trilleplay <trille@trilleplay.net>
2017-06-11 18:52:24 +0200

    Create qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -1,4 +1,5 @@
 <html>
 <img src=https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$uuid&choe=UTF-8 />
-$uuid=cookie.uuid
+$uuid=document.cookie.uuid
+document.cookie.uuid = "uuid=//BROKEN//";
 </html>
